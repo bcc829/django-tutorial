1859bc27849dca863ea6065dd83e96b131d07d66
jyjeong <jyjeong@mcnc.co.kr>
2019-12-10 17:39:38 +0900

    첫 번째 장고 앱 작성하기, part 7

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
